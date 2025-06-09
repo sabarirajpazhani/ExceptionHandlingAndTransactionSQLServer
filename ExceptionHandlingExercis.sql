b9d2e89c056094cb17d9776966f54109e5888b4c
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-06-09 17:32:29 +0530

    Question 8

--- ExceptionHandlingExercis.sql
+++ ExceptionHandlingExercis.sql
@@ -145,3 +145,166 @@ begin
 	end catch
 end
 
+/* 8.Savepoint with Error Recovery:
+Use SAVE TRANSACTION and simulate an error after it. Rollback to the savepoint and continue the transaction.*/
+begin transaction
+save transaction savePoint1
+begin try
+	insert into Employee
+	values ('Thamizh','Thamizh@gmail.com',80000,'9987875678')
+	Declare @Result3 int = 100/0
+	print 'Result - '+cast(@Result3 as varchar(80))
+end try
+begin catch
+	rollback transaction savePoint1
+	print 'Error - ' + error_message()
+end catch
+commit transaction
+
+select * from Employee;--create database for Expectoin Handling Question
+create database ExceptionHandlingExercise;
+
+--use database 
+use ExceptionHandlingExercise;
+
+--create table
+create table Employee(
+	EmpID int identity(1,1) primary key,
+	EmpName varchar(80),
+	EmpEmail varchar(80),
+	EmpSalary int,
+	EmpPhone varchar(20)
+);
+
+INSERT INTO Employee (EmpName, EmpEmail, EmpSalary, EmpPhone) VALUES
+('Arun Kumar', 'arun.kumar@example.com', 45000, '9876543210'),
+('Balaji M', 'balaji.m@example.com', 52000, '8765432109'),
+('Charan R', 'charan.r@example.com', 61000, '7654321890'),
+('Deepika S', 'deepika.s@example.com', 47000, '9090909090'),
+('Ganesh K', 'ganesh.k@example.com', 58000, '9123456789');
+
+select * from Employee;
+
+
+/*1. Basic TRY...CATCH Block:
+Write a script that inserts a record into a table and use a TRY...CATCH block to handle any errors.*/
+begin try
+	insert into Employee (EmpName,EmpEmail, EmpSalary, EmpPhone)
+	values
+	('Elango', 'Elango@gmail.com',85000, '9987875632');
+end try
+begin catch
+	print 'Error Occurred : ' + ERROR_MESSAGE()
+end catch
+
+/*2. Handle Division by Zero:
+Create a query that divides two numbers. Use TRY...CATCH to catch a "Divide by zero" error and print a custom message.*/
+begin try
+	declare @Number1 int = 180, @Number2 int = 0
+	declare @Result int
+	set @Result = @Number1 / @Number2
+	print 'Result - ' + cast(@Result as varchar(30))
+end try
+begin catch
+	print'Divide by Zero'
+	print error_number()
+end catch
+
+/*3. Log Error Details:
+Use ERROR_MESSAGE(), ERROR_LINE(), and ERROR_SEVERITY() inside a CATCH block to log complete error information into a separate ErrorLog table.*/
+create table ErrorLog(
+	ErrorMessage varchar(80),
+	ErrorLine int,
+	ErrorSeverity int,
+);
+
+drop table ErrorLog;
+
+begin try
+	declare @Num1 int = 100, @Num2 int = 0
+	declare @Result1 int = 0
+	set @Result1 = @Num1 /@Num2
+	print 'Result - ' + cast(@Result1 as varchar(30))
+	insert into Employee (EmpID, EmpName,EmpEmail, EmpSalary, EmpPhone) values
+	(6,'Faizal', 'Faizal@gmail.com',85000,'77656523566')
+	print 'Records are inserted Successfully'
+end try
+begin catch
+	declare @Message varchar(80) = error_message()
+	declare @Line int = error_line()
+	declare @severity int = error_severity()
+	insert into ErrorLog values
+	(@Message, @Line, @severity);
+end catch
+
+select * from ErrorLog;
+
+/*4. Nested TRY...CATCH:
+Write a script with a nested TRY...CATCH structure where an inner error is caught and rethrown to the outer block.*/
+begin try
+	begin try
+		select 100/0
+		print 'Successfully Divided'
+	end try
+	begin catch 
+		print 'Inner Catch '+ error_message()
+		throw;
+	end catch
+end try
+begin catch
+	print 'Outter Catch '+ error_message()
+end catch
+
+/*5. RAISEERROR with Custom Message:
+Use RAISEERROR inside the CATCH block to throw a user-defined error with severity level 16 and log it.*/
+begin try
+	declare @nums1 int = 150, @nums2 int = 0, @Result2 int = 0
+	set @Result2 = @nums1/@nums2;
+	print 'Result - ' + cast(@Result2 as varchar(30))
+end try
+begin catch
+	insert into ErrorLog values
+	(ERROR_MESSAGE(), ERROR_LINE(), ERROR_SEVERITY())
+end catch
+
+select * from ErrorLog;
+
+
+/*6. Transaction Rollback on Error:
+Simulate a transaction with multiple inserts, then force an error. Use TRY...CATCH to roll back the transaction and print "Transaction rolled back due to error."*/
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    INSERT INTO Employee (EmpName, EmpEmail, EmpSalary, EmpPhone) 
+    VALUES ('Raj', 'Raj@gmail.com', 75000, '9987875678');
+
+    INSERT INTO Employee (EmpName, EmpEmail, EmpSalary, EmpPhone) 
+    VALUES (NULL, 'Ravi@gmail.com', 80000, '776765677');  -- This will cause error if EmpName is NOT NULL
+
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+
+    PRINT 'Error: ' + ERROR_MESSAGE();
+    PRINT 'Error Number: ' + CAST(ERROR_NUMBER() AS VARCHAR);
+END CATCH;
+
+/*7. Stored Procedure with Exception Handling:
+Create a stored procedure that performs insert/update and includes error handling using TRY...CATCH.*/
+create procedure spInsert
+	@EmpName varchar(80) ,
+	@EmpEmail varchar(80),
+	@EmpSalary int,
+	@EmpPhone varchar(20)
+as
+begin
+	begin try
+		insert into Employee values
+		(@EmpName,@EmpEmail,@EmpSalary,@EmpPhone)
+	end try
+	begin catch
+		Print 'Error ' + error_message()
+	end catch
+end
+
